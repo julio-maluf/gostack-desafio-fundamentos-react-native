8fe8ea03f7994ac9ed54097063a151e3f89ea7db
Julio Maluf <julio_maluf@hotmail.com>
2020-11-22 23:36:38 -0300

    challenge finalized

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  // baseURL: 'http://localhost:3333',
+  baseURL: 'http://172.22.73.204:3333',
 });
 
 export default api;
